31d2e3b8880c01c60f0a017037b9f72e79d33336
Yusuf-Aksakal <aksakalyusuf01@gmail.com>
2026-08-21 10:35:42 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,12 @@ import os
 import requests
 import tweepy
 
-# GitHub Secrets'tan anahtarları çekiyoruz
 API_KEY = os.environ.get("TWITTER_API_KEY")
 API_SECRET = os.environ.get("TWITTER_API_SECRET")
 ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
 ACCESS_SECRET = os.environ.get("TWITTER_ACCESS_SECRET")
 
 def send_tweet():
-    # GamerPower API'den güncel oyun fırsatını al
     url = "https://www.gamerpower.com/api/giveaways"
     response = requests.get(url)
     
@@ -22,16 +20,13 @@ def send_tweet():
         print("Aktif fırsat bulunamadı.")
         return
 
-    # En güncel fırsatı al
     latest_deal = deals[0]
     title = latest_deal.get("title")
     giveaway_url = latest_deal.get("open_giveaway_url")
     platforms = latest_deal.get("platforms")
 
-    # Tweet metnini oluştur
     tweet_text = f"🔥 Ücretsiz Oyun Fırsatı!\n\n🎮 {title}\n🕹️ Platform: {platforms}\n\n🔗 Hemen Al: {giveaway_url}\n\n#ÜcretsizOyun #OyunFırsatı #Gaming"
 
-    # X API v2 Bağlantısı
     client = tweepy.Client(
         consumer_key=API_KEY,
         consumer_secret=API_SECRET,
